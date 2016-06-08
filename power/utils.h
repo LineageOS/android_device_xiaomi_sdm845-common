af29d6f96a52da498020c4edf3ea05c949f713e5
Wei Wang <wvw@google.com>
2016-06-07 18:44:31 -0700

    sdm845-common: power: Use monotonic time for interaction boost
    Using the wall clock will cause boosts to be disabled
    when/if the clock is adjusted backward.
    
    Bug: 29191415
    Bug: 29208304
    Change-Id: I8af5f40b46d996ce7bccb8324fc186e2f3a5b267
    (cherry picked from commit 2bf13f822cf39123ee0e97e40caf1288ba4f457a)

--- power/utils.h
+++ power/utils.h
@@ -44,3 +44,5 @@ void undo_hint_action(int hint_id);
 void release_request(int lock_handle);
 int interaction_with_handle(int lock_handle, int duration, int num_args, int opt_list[]);
 int perf_hint_enable(int hint_id, int duration);
+
+long long calc_timespan_us(struct timespec start, struct timespec end);
