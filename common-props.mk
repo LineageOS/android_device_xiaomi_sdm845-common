943199b308c401183348ad5a1bbdb506829797a4
Bruno Martins <bgcngm@gmail.com>
2018-09-21 15:40:34 +0100

    sdm845-common: Update common system properties for P
    Change-Id: Iaaef416a86ecb43e145906a93d8b027db565bf81

--- common-props.mk
+++ common-props.mk
@@ -4,36 +4,22 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # CNE and DPM
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.cne.feature=1 \
+    persist.vendor.cne.feature=1 \
     persist.vendor.dpm.feature=1 \
     persist.vendor.dpm.nsrm.bkg.evt=3955
 
 # Data modules
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.data.df.agg.dl_pkt=10 \
-    persist.data.df.agg.dl_size=4096 \
     persist.data.df.dev_name=rmnet_usb0 \
-    persist.data.df.dl_mode=5 \
-    persist.data.df.iwlan_mux=9 \
-    persist.data.df.mux_count=8 \
-    persist.data.df.ul_mode=5 \
-    persist.data.mode=concurrent \
-    persist.data.netmgrd.qos.enable=true \
-    persist.data.profile_update=true \
-    persist.data.wda.enable=true \
-    persist.rmnet.data.enable=true \
-    ro.use_data_netmgrd=true
+    persist.vendor.data.mode=concurrent \
+    ro.vendor.use_data_netmgrd=true
 
 # Graphics
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.enable_hwc_vds=1 \
-    debug.sf.latch_unsignaled=1 \
     persist.debug.wfd.enable=1 \
-    persist.demo.hdmirotationlock=false \
-    sdm.debug.disable_dest_scalar=1 \
     sdm.debug.disable_inline_rotator=1 \
-    sdm.debug.disable_inline_rotator_secure=1 \
-    sdm.debug.prefersplit=1
+    sdm.debug.disable_inline_rotator_secure=1
 
 # Netflix custom property
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -41,22 +27,22 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Perf
 PRODUCT_PROPERTY_OVERRIDES += \
-    iop.enable_prefetch_ofr=1 \
-    iop.enable_uxe=0 \
+    vendor.iop.enable_prefetch_ofr=1 \
+    vendor.iop.enable_uxe=1 \
     ro.vendor.qti.core_ctl_min_cpu=2 \
     ro.vendor.qti.core_ctl_max_cpu=4
 
 # RCS and IMS
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.data.iwlan.enable=true \
+    persist.radio.VT_HYBRID_ENABLE=1 \
     persist.rcs.supported=0 \
     persist.vendor.ims.disableUserAgent=0
 
 # RIL
 PRODUCT_PROPERTY_OVERRIDES += \
     DEVICE_PROVISIONED=1 \
-    persist.radio.atfwd.start=true \
     persist.radio.flexmap_type=none \
+    persist.vendor.radio.atfwd.start=true \
     persist.vendor.radio.force_on_dc=true \
     persist.vendor.radio.redir_party_num=1 \
     ril.subscription.types=NV,RUIM \
@@ -65,5 +51,5 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # SSR
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.sys.ssr.enable_ramdumps=1 \
-    persist.sys.ssr.restart_level=ALL_ENABLE
+    persist.vendor.ssr.enable_ramdumps=1 \
+    persist.vendor.ssr.restart_level=ALL_ENABLE
