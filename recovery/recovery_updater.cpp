b8266365b1e7562e607a7a96e2889ef80c0b64f9
Akhil Narang <akhilnarang.1999@gmail.com>
2018-10-31 21:16:46 +0530

    sdm845-common: Add vendor check to librecovery_updater
    This adds vendor image version assertions, so that the OTA package
    is not installed unless the recommended vendor (and matching firmware)
    or newer is present in the device.
    
    The checks that are now in place are: modem timestamp, vendor build date
    (in UTC format) and corresponding VNDK version.
    
    Change-Id: I1aef57d03ee86040f2eb3a02f79cc3f00ae9cda3
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- recovery/recovery_updater.cpp
+++ recovery/recovery_updater.cpp
@@ -41,6 +41,13 @@
 #define MODEM_VER_STR "Time_Stamp\": \""
 #define MODEM_VER_STR_LEN 14
 #define MODEM_VER_BUF_LEN 20
+#define VENDOR_PART_PATH "/dev/block/bootdevice/by-name/vendor"
+#define VENDOR_VER_STR "o.vendor.build.date.utc="
+#define VENDOR_VER_STR_LEN 24
+#define VENDOR_VER_BUF_LEN 11
+#define VNDK_VER_STR "o.vndk.version="
+#define VNDK_VER_STR_LEN 15
+#define VNDK_VER_BUF_LEN 3
 
 /* Boyer-Moore string search implementation from Wikipedia */
 
@@ -120,40 +127,40 @@ static char * bm_search(const char *str, size_t str_len, const char *pat,
     return NULL;
 }
 
-static int get_modem_version(char *ver_str, size_t len) {
+static int get_version(char *ver_str, size_t len, char *PART_PATH, char *VER_STR, size_t VER_STR_LEN) {
     int ret = 0;
     int fd;
-    int modem_size;
-    char *modem_data = NULL;
+    int size;
+    char *data = NULL;
     char *offset = NULL;
 
-    fd = open(MODEM_PART_PATH, O_RDONLY);
+    fd = open(PART_PATH, O_RDONLY);
     if (fd < 0) {
         ret = errno;
         goto err_ret;
     }
 
-    modem_size = lseek64(fd, 0, SEEK_END);
-    if (modem_size == -1) {
+    size = lseek64(fd, 0, SEEK_END);
+    if (size == -1) {
         ret = errno;
         goto err_fd_close;
     }
 
-    modem_data = (char *) mmap(NULL, modem_size, PROT_READ, MAP_PRIVATE, fd, 0);
-    if (modem_data == (char *)-1) {
+    data = (char *) mmap(NULL, size, PROT_READ, MAP_PRIVATE, fd, 0);
+    if (data == (char *)-1) {
         ret = errno;
         goto err_fd_close;
     }
 
-    /* Do Boyer-Moore search across MODEM data */
-    offset = bm_search(modem_data, modem_size, MODEM_VER_STR, MODEM_VER_STR_LEN);
+    /* Do Boyer-Moore search across data */
+    offset = bm_search(data, size, VER_STR, VER_STR_LEN);
     if (offset != NULL) {
-        snprintf(ver_str, len, "%s", offset + MODEM_VER_STR_LEN);
+        snprintf(ver_str, len, "%s", offset + VER_STR_LEN);
     } else {
         ret = -ENOENT;
     }
 
-    munmap(modem_data, modem_size);
+    munmap(data, size);
 err_fd_close:
     close(fd);
 err_ret:
@@ -167,7 +174,7 @@ Value* VerifyModemFn(const char* name, State* state,
     int ret;
     struct tm tm1, tm2;
 
-    ret = get_modem_version(current_modem_version, MODEM_VER_BUF_LEN);
+    ret = get_version(current_modem_version, MODEM_VER_BUF_LEN, MODEM_PART_PATH, MODEM_VER_STR, MODEM_VER_STR_LEN);
     if (ret) {
         return ErrorAbort(state, kVendorFailure,
                 "%s() failed to read current MODEM build time-stamp: %d", name, ret);
@@ -195,6 +202,50 @@ Value* VerifyModemFn(const char* name, State* state,
     return StringValue(strdup(ret ? "1" : "0"));
 }
 
+/* verify_vendor("VENDOR_VERSION", "VENDOR_VERSION", ...) */
+Value* VerifyVendorFn(const char* name, State* state,
+                     const std::vector<std::unique_ptr<Expr>>& argv) {
+    char current_vendor_version[VENDOR_VER_BUF_LEN];
+    char current_vndk_version[VNDK_VER_BUF_LEN];
+    int ret;
+
+    // Check for build date
+    ret = get_version(current_vendor_version, VENDOR_VER_BUF_LEN, VENDOR_PART_PATH, VENDOR_VER_STR, VENDOR_VER_STR_LEN);
+    if (ret) {
+        return ErrorAbort(state, kVendorFailure,
+                "%s() failed to read current vendor build date: %d", name, ret);
+    }
+
+    std::vector<std::string> args;
+    if (!ReadArgs(state, argv, &args)) {
+        return ErrorAbort(state, kArgsParsingFailure, "%s() error parsing arguments", name);
+    }
+
+    ret = 0;
+    if (std::stoi(current_vendor_version) >= std::stoi(args[0])) {
+            ret = 1;
+    }
+
+    // Return now if build date didn't match
+    if (!ret) {
+        return StringValue(strdup("0"));
+    }
+
+    // Check for VNDK version
+    ret = get_version(current_vndk_version, VNDK_VER_BUF_LEN, VENDOR_PART_PATH, VNDK_VER_STR, VNDK_VER_STR_LEN);
+    if (ret) {
+        return ErrorAbort(state, kVendorFailure,
+                "%s() failed to read current vendor VNDK version: %d", name, ret);
+    }
+
+    ret = 0;
+    if (std::stoi(current_vndk_version) == std::stoi(args[1])) {
+        ret = 1;
+    }
+    return StringValue(strdup(ret ? "1" : "0"));
+}
+
 void Register_librecovery_updater_xiaomi() {
     RegisterFunction("xiaomi.verify_modem", VerifyModemFn);
+    RegisterFunction("xiaomi.verify_vendor", VerifyVendorFn);
 }
