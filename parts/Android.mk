583dff54f9fa1277ef57d84d19db7c26d22fcc5c
Joey <joey@lineageos.org>
2018-12-28 11:33:30 +0100

    xiaomi-sdm845-common: add dirac preferences
    Inject into sound preferences through settings IA
    
    Change-Id: I37f6e64fa440c43dbe238cc65af5f9b0bbd60d83
    Signed-off-by: Joey <joey@lineageos.org>

--- parts/Android.mk
+++ parts/Android.mk
@@ -6,7 +6,7 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
-LOCAL_PACKAGE_NAME := XiaomiDoze
+LOCAL_PACKAGE_NAME := XiaomiParts
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVATE_PLATFORM_APIS := true
 LOCAL_PRIVILEGED_MODULE := true
---
+++ parts/src/org/lineageos/settings/dirac/DiracSettingsFragment.java
@@ -0,0 +1,145 @@
+/*
+ * Copyright (C) 2018 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.settings.dirac;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v14.preference.PreferenceFragment;
+import android.support.v14.preference.SwitchPreference;
+import android.support.v14.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceCategory;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
+
+public class DiracSettingsFragment extends PreferenceFragment implements
+        OnPreferenceChangeListener, CompoundButton.OnCheckedChangeListener {
+
+    private static final String PREF_HEADSET = "dirac_headset_pref";
+    private static final String PREF_PRESET = "dirac_preset_pref";
+
+    private ListPreference mHeadsetType;
+    private ListPreference mPreset;
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        addPreferencesFromResource(R.xml.dirac_settings);
+        final ActionBar actionBar = getActivity().getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        boolean enhancerEnabled = Utils.isDiracEnabled(getActivity());
+
+        mHeadsetType = (SwitchPreference) findPreference(PREF_HEADSET);
+        mHeadsetType.setOnPreferenceChangeListener(this);
+        mHeadsetType.setEnabled(enhancerEnabled);
+        // TODO: adapt to real values
+        String[] headsetEntries = new String[] {
+            getString(R.string.dirac_headset_0),
+            getString(R.string.dirac_headset_1),
+            getString(R.string.dirac_headset_2),
+            getString(R.string.dirac_headset_3)
+        };
+        String[] headSetValues = new String[] {
+            "0", "1", "2", "3"
+        };
+        mHeadsetType.setEntries(headsetEntries);
+        mHeadsetType.setEntryValues(headSetValues);
+
+        mPreset = (SwitchPreference) findPreference(PREF_PRESET);
+        mPreset.setOnPreferenceChangeListener(this);
+        mPreset.setEnabled(enhancerEnabled);
+        // TODO: adapt to real values
+        String[] presetEntries = new String[] {
+            getString(R.string.dirac_preset_0),
+            getString(R.string.dirac_preset_1),
+            getString(R.string.dirac_preset_2),
+            getString(R.string.dirac_preset_3)
+        };
+        String[] preSetValues = new String[] {
+            "0", "1", "2", "3"
+        };
+        mpresetType.setEntries(presetEntries);
+        mpresetType.setEntryValues(preSetValues);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        final View view = LayoutInflater.from(getContext()).inflate(R.layout.dirac, container, false);
+        ((ViewGroup) view).addView(super.onCreateView(inflater, container, savedInstanceState));
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        boolean enhancerEnabled = Utils.isDiracEnabled(getActivity());
+
+        mTextView = view.findViewById(R.id.switch_text);
+        mTextView.setText(getString(enhancerEnabled ?
+                R.string.switch_bar_on : R.string.switch_bar_off));
+
+        View switchBar = view.findViewById(R.id.switch_bar);
+        Switch switchWidget = switchBar.findViewById(android.R.id.switch_widget);
+        switchWidget.setChecked(enhancerEnabled);
+        switchWidget.setOnCheckedChangeListener(this);
+        switchBar.setOnClickListener(v -> switchWidget.setChecked(!switchWidget.isChecked()));
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        switch (preference.getKey()) {
+            case PREF_HEADSET:
+                // TODO: on Headset changed
+                return true;
+            case PREF_PRESET:
+                // TODO: on Preset changed
+                return true;
+            default: return false;
+        }
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+        // TODO: Toggle enhacer
+
+        mTextView.setText(getString(isChecked ? R.string.switch_bar_on : R.string.switch_bar_off));
+
+        mHeadsetType.setEnabled(isChecked);
+        mPreset.setEnabled(isChecked);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            getActivity().onBackPressed();
+            return true;
+        }
+        return false;
+    }
+}
