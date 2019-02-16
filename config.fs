29ceac08560d131febefac4b8afd62272422e691
Bruno Martins <bgcngm@gmail.com>
2019-02-16 18:54:05 +0000

    [DNM] sdm845-common: Let prebuilt vendors die in hell
    Change-Id: I18510ccfa877c1ecde1aed6eccd3bd701c95cd8a

--- config.fs
+++ config.fs
@@ -21,8 +21,8 @@ caps: BLOCK_SUSPEND NET_ADMIN
 
 [vendor/bin/cnss-daemon]
 mode: 0755
-user: AID_BLUETOOTH
-group: AID_BLUETOOTH
+user: AID_SYSTEM
+group: AID_SYSTEM
 caps: NET_BIND_SERVICE
 
 [vendor/bin/pm-service]
