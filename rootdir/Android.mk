c82c80c34bdad65f8195ddc2b9838fb740ce749c
Bruno Martins <bgcngm@gmail.com>
2019-02-17 16:13:13 +0000

    sdm845-common: Include OEM specific scripts and pick custom init changes
     * From MIUI 9.2.15
    
    Change-Id: I1b72c111171b7fb226e7c34a779c139707b1b841

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -114,3 +114,20 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.qti.qseecomd.sh
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
+
+# Xiaomi SDM845 targets
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.leds.sh
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.leds.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.panel_info.sh
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.panel_info.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
+include $(BUILD_PREBUILT)
