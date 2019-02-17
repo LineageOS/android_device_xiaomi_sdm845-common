be86a02efaf081c0308902d4f4c772b9e4316a16
Bruno Martins <bgcngm@gmail.com>
2019-02-17 16:13:13 +0000

    sdm845-common: Add OEM specific scripts and merge custom init changes
     * From MIUI 9.2.15
    
    Change-Id: I1b72c111171b7fb226e7c34a779c139707b1b841

--- rootdir/etc/init.qcom.sh
+++ rootdir/etc/init.qcom.sh
@@ -84,29 +84,8 @@ start_msm_irqbalance_8939()
 {
 	if [ -f /vendor/bin/msm_irqbalance ]; then
 		case "$platformid" in
-		    "239" | "293" | "294" | "295" | "304" | "338" | "313" |"353")
+		    "239" | "293" | "294" | "295" | "304" | "313")
 			start vendor.msm_irqbalance;;
-		    "349" | "350" )
-			start vendor.msm_irqbal_lb;;
-		esac
-	fi
-}
-
-start_msm_irqbalance_msmnile()
-{
-         if [ -f /vendor/bin/msm_irqbalance ]; then
-                start vendor.msm_irqbalance
-         fi
-}
-
-start_msm_irqbalance660()
-{
-	if [ -f /vendor/bin/msm_irqbalance ]; then
-		case "$platformid" in
-		    "317" | "324" | "325" | "326" | "345" | "346")
-			start vendor.msm_irqbalance;;
-		    "318" | "327" | "385")
-			start vendor.msm_irqbl_sdm630;;
 		esac
 	fi
 }
@@ -197,7 +176,7 @@ case "$target" in
         fi
 
         case "$soc_id" in
-             "317" | "324" | "325" | "326" | "318" | "327" | "385" )
+             "317" | "324" | "325" | "326" | "318" | "327" )
                   case "$hw_platform" in
                        "Surf")
                                     setprop qemu.hw.mainkeys 0
@@ -214,7 +193,7 @@ case "$target" in
                   esac
                   ;;
        esac
-        start_msm_irqbalance660
+        start_msm_irqbalance
         ;;
     "apq8084")
         platformvalue=`cat /sys/devices/soc0/hw_platform`
@@ -267,7 +246,7 @@ case "$target" in
                   ;;
         esac
         ;;
-    "msm8994" | "msm8992" | "msm8998" | "apq8098_latv" | "sdm845" | "sdm710" | "qcs605" | "sm6150" | "trinket")
+    "msm8994" | "msm8992" | "msm8998" | "apq8098_latv" | "sdm845" | "sdm710" | "qcs605" | "msmnile" | "msmsteppe")
         start_msm_irqbalance
         ;;
     "msm8996")
@@ -293,9 +272,6 @@ case "$target" in
         ;;
     "msm8909")
         start_vm_bms
-        ;;
-    "msmnile")
-        start_msm_irqbalance_msmnile
         ;;
     "msm8937")
         start_msm_irqbalance_8939
@@ -376,7 +352,7 @@ case "$target" in
              hw_platform=`cat /sys/devices/system/soc/soc0/hw_platform`
         fi
         case "$soc_id" in
-             "336" | "337" | "347" | "360" | "393" )
+             "336" | "337" | "347" | "360" )
                   case "$hw_platform" in
                        "Surf")
                                     setprop qemu.hw.mainkeys 0
