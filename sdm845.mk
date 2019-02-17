0df2d483f70eeb3cec3352b9dadd87846671d19d
Bruno Martins <bgcngm@gmail.com>
2019-02-17 16:13:13 +0000

    sdm845-common: Include OEM specific scripts and pick custom init changes
     * From MIUI 9.2.15
    
    Change-Id: I1b72c111171b7fb226e7c34a779c139707b1b841

--- sdm845.mk
+++ sdm845.mk
@@ -82,7 +82,9 @@ PRODUCT_PACKAGES += \
 # Common init scripts
 PRODUCT_PACKAGES += \
     init.class_main.sh \
+    init.leds.sh \
     init.msm.usb.configfs.rc \
+    init.panel_info.sh \
     init.qcom.class_core.sh \
     init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
@@ -93,6 +95,7 @@ PRODUCT_PACKAGES += \
     init.qcom.usb.sh \
     init.qti.qseecomd.sh \
     init.recovery.qcom.rc \
+    init.remosaic.rc \
     init.target.rc \
     ueventd.qcom.rc
 
