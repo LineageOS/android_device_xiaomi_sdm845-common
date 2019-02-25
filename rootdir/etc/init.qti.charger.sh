65d4a77b82d1a1542337ac8700da3d88d9574ee6
Ashay Jaiswal <ashayj@codeaurora.org>
2019-02-25 17:09:14 +0530

    sdm845-common: Add script to update file permission
    Update file ownership of all the device files and power supplies
    exported by Charger/FG/QG/USBPD drivers to "system" user/group.
    
    Change-Id: I8149f20b8b48ac8f460e6b25b53225e1d9371c43

---
+++ rootdir/etc/init.qti.charger.sh
@@ -0,0 +1,47 @@
+#! /vendor/bin/sh
+
+# Copyright (c) 2019 The Linux Foundation. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of The Linux Foundation nor
+#       the names of its contributors may be used to endorse or promote
+#       products derived from this software without specific prior written
+#       permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NON-INFRINGEMENT ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+export PATH=/vendor/bin
+
+prefix="/sys/class/"
+#List of folder for ownership update
+arr=( "power_supply/battery/" "power_supply/usb/" "power_supply/main/" "power_supply/charge_pump_master/" "power_supply/pc_port/" "power_supply/dc/" "power_supply/bms/" "power_supply/parallel/" "usbpd/usbpd0/" "qc-vdm/" "charge_pump/" "qcom-battery/" )
+for i in "${arr[@]}"
+do
+    for j in `ls "$prefix""$i"`
+    do
+        #skip directories to prevent possible security issues.
+        if [[ -d "$prefix""$i""$j" ]]
+        then
+            continue
+        else
+            chown -h system.system "$prefix""$i""$j"
+        fi
+    done
+done
--- sdm845.mk
+++ sdm845.mk
@@ -109,6 +109,7 @@ PRODUCT_PACKAGES += \
     init.qcom.sh \
     init.qcom.usb.rc \
     init.qcom.usb.sh \
+    init.qti.charger.sh \
     init.qti.qseecomd.sh \
     init.recovery.qcom.rc \
     ueventd.qcom.rc
