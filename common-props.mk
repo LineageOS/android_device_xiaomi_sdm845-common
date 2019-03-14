76eefc8e8384d724dd768179b5103f63f1da9e9b
Bruno Martins <bgcngm@gmail.com>
2019-03-14 11:59:53 +0100

    sdm845-common: Set up Bluetooth
    Change-Id: I2057e44919354d4cdf172dfbc85da432b32419f0

--- common-props.mk
+++ common-props.mk
@@ -7,7 +7,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
-    vendor.bluetooth.soc=cherokee
+    vendor.qcom.bluetooth.soc=cherokee
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
