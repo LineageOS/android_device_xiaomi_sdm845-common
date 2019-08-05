6663582b6d717e07ae60a20c3c491f3b7e73ab02
Akhil Narang <akhilnarang.1999@gmail.com>
2019-08-05 20:07:03 +0530

    sdm845-common: releasetools: be more clear about firmware image patching
    This looks very odd on most of the devices using this tree, because nothing is being patched
    Just mention which image is being flashed and only when it actually is
    
    Change-Id: Ib6c35e08aee0f1c102da28ca37434093a92f7045
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- releasetools.py
+++ releasetools.py
@@ -37,14 +37,14 @@ def IncrementalOTA_Assertions(info):
 def AddImage(info, basename, dest):
   path = "IMAGES/" + basename
   if path not in info.input_zip.namelist():
-    return
+    pass
 
-  data = info.input_zip.read(path)
-  common.ZipWriteStr(info.output_zip, basename, data)
+  #data = info.input_zip.read(path)
+  #common.ZipWriteStr(info.output_zip, basename, data)
+  info.script.Print("Flashing {} image".format(dest.split('/')[-1]))
   info.script.AppendExtra('package_extract_file("%s", "%s");' % (basename, dest))
 
 def OTA_InstallEnd(info):
-  info.script.Print("Patching firmware images...")
   AddImage(info, "dtbo.img", "/dev/block/bootdevice/by-name/dtbo")
   return
 
