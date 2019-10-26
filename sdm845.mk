70ebbcf3b7e0b949b814c33284c93d376502b316
argraur <argraur@gmail.com>
2019-10-26 17:08:09 +0000

    sdm845-common: Build vendor variant of libdng_sdk for watermark
    Signed-off-by: argraur <argraur@gmail.com>
    Change-Id: Id5d4a73d29418bd3b7c0c9f92e4d5efd8be68c3b

--- sdm845.mk
+++ sdm845.mk
@@ -105,6 +105,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
+    libdng_sdk.vendor \
     Snap \
     vendor.qti.hardware.camera.device@1.0.vendor
 
