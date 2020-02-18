13df1569340c19780bdc241ae8384b789ba3b1d1
YumeMichi <do4suki@gmail.com>
2020-02-19 04:24:29 +0800

    sdm845-common: Switch to OSS audio HAL
    Change-Id: Ib5cf3b1e971082c56ac7c8cfe0a3b8980d3986bc
    Signed-off-by: YumeMichi <do4suki@gmail.com>

--- sdm845.mk
+++ sdm845.mk
@@ -74,11 +74,20 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@5.0-impl \
     android.hardware.soundtrigger@2.2-impl \
     audio.a2dp.default \
+    audio.primary.sdm845 \
     audio.r_submix.default \
     audio.usb.default \
+    liba2dpoffload \
+    libbatterylistener \
+    libcomprcapture \
+    libexthwplugin \
+    libhdmiedid \
+    libhfp \
     libqcompostprocbundle \
     libqcomvisualizer \
     libqcomvoiceprocessing \
+    libsndmonitor \
+    libspkrprot \
     libtinycompress \
     libvolumelistener \
     tinymix
