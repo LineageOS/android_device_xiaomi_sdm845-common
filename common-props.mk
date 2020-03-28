3cd3ad7f36d8090c115f8a1cb56f3a9982beee4e
Han Wang <416810799@qq.com>
2020-03-28 14:34:59 +0100

    Revert "sdm845-common: enable GL comp backpressure"
    This reverts commit c9709ea06184a46ea42af0ae6094ddaf886f96f2.
    
    Change-Id: I74c5e4ba830ef23448e4f16822687d7a22746a3b

--- common-props.mk
+++ common-props.mk
@@ -135,7 +135,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.egl.hw=0 \
     debug.sf.disable_backpressure=1 \
-    debug.sf.enable_gl_backpressure=1 \
     debug.sf.enable_hwc_vds=1 \
     debug.sf.latch_unsignaled=1 \
     debug.sf.hw=0 \
