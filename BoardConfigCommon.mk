7af2e93a0a37de9ad93a4c2989ccade6a5d0dca2
Felix <google@ix5.org>
2020-05-22 00:45:29 +0200

    sdm845-common: Use SYSTEM_*_SEPOLICY_DIRS
    The old variable name is being deprecated on API level 31+.
    
    See https://android-review.googlesource.com/q/topic:"board-system-ext-sepolicy"
    
    Signed-off-by: Felix <google@ix5.org>
    Change-Id: I9b65f43060d503ab2d05ced8742b3dcce699c8f5

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -149,8 +149,8 @@ VENDOR_SECURITY_PATCH := 2020-05-01
 # SELinux
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/public
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 
 # WiFi
