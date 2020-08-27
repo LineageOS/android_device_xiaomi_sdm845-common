3e231a6e0851fa7170b431c107737271ace41d3b
Dhina17 <dhinalogu@gmail.com>
2020-08-27 14:56:10 +0530

    sdm845-common: Drop unused BOARD_KERNEL_TAGS_OFFSET
    Change-Id: Ic5484fb41eec29f2d0f5eee52022e84715df7221

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -34,7 +34,6 @@ BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0xA84000 androidboot.hardware=qcom androidboot.console=ttyMSM0 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 service_locator.enable=1 swiotlb=2048 androidboot.configfs=true loop.max_part=7 androidboot.usbcontroller=a600000.dwc3
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 4096
-BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_RAMDISK_OFFSET := 0x01000000
 TARGET_KERNEL_ARCH := arm64
 ifeq ($(TARGET_PREBUILT_KERNEL),)
