7a908663e13853d804b494e88f969b444d7066bf
Chirayu Desai <chirayudesai1@gmail.com>
2020-11-12 22:01:27 +0530

    sdm845-common: pocketmode: Set SDK_VERSION to current
    Fixes: Specifies both LOCAL_SDK_VERSION (system_current) and
    LOCAL_PRIVATE_PLATFORM_APIS (true) but should specify only one.
    
    Change-Id: I1d37f7408cbe73f4bc9e7c278c7a58f72dc8e1f0

--- pocketmode/Android.mk
+++ pocketmode/Android.mk
@@ -8,7 +8,7 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_PACKAGE_NAME := XiaomiPocketMode
 LOCAL_CERTIFICATE := platform
-LOCAL_PRIVATE_PLATFORM_APIS := true
+LOCAL_SDK_VERSION := current
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_VENDOR_MODULE := true
 
