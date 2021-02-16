8d92bd277b3b99e290a643631197c7fc1cb44ea3
Bruno Martins <bgcngm@gmail.com>
2021-02-16 13:44:18 +0000

    sdm845-common: Migrate to Xiaomi power AIDL HAL
    Change-Id: I42170d51a517170b58d532addd9c38496e43457c

--- power/power-mode.cpp
+++ power/power-mode.cpp
@@ -57,10 +57,11 @@ int open_ts_input() {
 }  // anonymous namespace
 
 namespace aidl {
-namespace android {
+namespace google {
 namespace hardware {
 namespace power {
 namespace impl {
+namespace pixel {
 
 static constexpr int kInputEventWakeupModeOff = 4;
 static constexpr int kInputEventWakeupModeOn = 5;
@@ -99,8 +100,9 @@ bool setDeviceSpecificMode(Mode type, bool enabled) {
     }
 }
 
+}  // namespace pixel
 }  // namespace impl
 }  // namespace power
 }  // namespace hardware
-}  // namespace android
+}  // namespace google
 }  // namespace aidl
