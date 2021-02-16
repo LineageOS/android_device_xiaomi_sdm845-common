0f5da1f3ac2454d56d9f2e31bf569f754a095aa8
Bruno Martins <bgcngm@gmail.com>
2021-02-16 13:44:18 +0000

    sdm845-common: Migrate to Xiaomi power HAL AIDL
    Change-Id: I42170d51a517170b58d532addd9c38496e43457c

--- sdm845.mk
+++ sdm845.mk
@@ -248,7 +248,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.3-service.xiaomi_sdm845-libperfmgr
+    android.hardware.power-service.xiaomi-libperfmgr
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/power/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
@@ -292,6 +292,8 @@ PRODUCT_PACKAGES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
+    hardware/google/interfaces \
+    hardware/google/pixel \
     hardware/xiaomi
 
 # Telephony
