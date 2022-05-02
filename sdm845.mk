654beafa99a856d2b0183c4f08041846a374b001
Bruno Martins <bgcngm@gmail.com>
2022-05-02 11:28:36 +0100

    sdm845-common: Move to common fingerprint HIDL
    Change-Id: I7cb0a45c865d9dbfd800b1d633f8d5ae613bb1b9

--- sdm845.mk
+++ sdm845.mk
@@ -168,7 +168,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.xiaomi_sdm845
+    android.hardware.biometrics.fingerprint@2.3-service.xiaomi
 
 # Gatekeeper
 PRODUCT_PACKAGES += \
