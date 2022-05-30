b7e64ec03c1304b0660d50b396500b1aeca716e9
Bruno Martins <bgcngm@gmail.com>
2022-05-30 19:27:51 +0100

    sdm845-common: Drop default DRM service
    This is not needed, since Widevine DRM and Clearkey DRM
    services are available.
    
    Change-Id: I97023f1bab88ddb8fa4cb205df190c516a759951

--- sdm845.mk
+++ sdm845.mk
@@ -167,8 +167,6 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl:64 \
-    android.hardware.drm@1.0-service \
     android.hardware.drm@1.3.vendor \
     android.hardware.drm@1.4-service.clearkey
 
