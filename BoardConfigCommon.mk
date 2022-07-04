10f913652f9361da736652a0a405375d39953335
Bruno Martins <bgcngm@gmail.com>
2022-07-04 23:42:23 +0100

    sdm845-common: Define main kernel defconfig
    Targets will append config fragments.
    
    Change-Id: I744d56d714add1c1864746276c554bdb5086a7a9

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -37,6 +37,7 @@ BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_CONFIG := vendor/xiaomi/mi845_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sdm845
 
 # Platform
