feb1f4bc2f4b29bd2eb6cf92ae07f2377c77125f
Bruno Martins <bgcngm@gmail.com>
2022-08-29 12:08:07 +0100

    sdm845-common: Move back to AOSP BT stack
    Change-Id: Idce87bbeb70eccb886a99c52055e6a1833a85276

--- bluetooth/include/bdroid_buildcfg.h
+++ bluetooth/include/bdroid_buildcfg.h
@@ -68,9 +68,8 @@ static inline const char *BtmGetDefaultName()
 #define BTM_DEF_LOCAL_NAME BtmGetDefaultName()
 // Disables read remote device feature
 #define MAX_ACL_CONNECTIONS   16
-#define MAX_L2CAP_CHANNELS    32
+#define MAX_L2CAP_CHANNELS    16
 #define BLE_VND_INCLUDED   TRUE
-#define GATT_MAX_PHY_CHANNEL  10
-#define AVDT_NUM_SEPS 35
+#define AVDT_NUM_SEPS 12
 
 #endif
