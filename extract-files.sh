e6a4f83fb70e1da5262744b9cbc112c058eb871e
Bruno Martins <bgcngm@gmail.com>
2022-09-06 13:43:13 +0100

    sdm845-common: Update DPM from caymanslm
    Change-Id: I1748fb7ceffe6b91f17ae546a53d84f5ea9bd18f

--- extract-files.sh
+++ extract-files.sh
@@ -23,13 +23,6 @@ source "${HELPER}"
 
 function blob_fixup() {
     case "${1}" in
-        system_ext/etc/init/dpmd.rc)
-            sed -i "s/\/system\/product\/bin\//\/system\/system_ext\/bin\//g" "${2}"
-            ;;
-        system_ext/etc/permissions/com.qti.dpmframework.xml)
-            ;&
-        system_ext/etc/permissions/dpmapi.xml)
-            ;&
         system_ext/etc/permissions/qcrilhook.xml)
             ;&
         system_ext/etc/permissions/telephonyservice.xml)
@@ -37,9 +30,6 @@ function blob_fixup() {
             ;;
         system_ext/etc/permissions/qti_libpermissions.xml)
             sed -i "s/name=\"android.hidl.manager-V1.0-java/name=\"android.hidl.manager@1.0-java/g" "${2}"
-            ;;
-        system_ext/lib64/libdpmframework.so)
-            sed -i "s/libhidltransport.so/libcutils-v29.so\x00\x00\x00/" "${2}"
             ;;
         system_ext/lib64/lib-imsvideocodec.so)
             ${PATCHELF} --add-needed "lib-imsvtshim.so" "${2}"
