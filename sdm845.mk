2dba908249ab2688949eb781cb8fc85f81b72590
Bruno Martins <bgcngm@gmail.com>
2022-09-09 14:43:56 +0100

    sdm845-common: Nuke TZ assertions
    Change-Id: Ie403f42c573b7ee0341af8a3a148ff74fb280b74

--- sdm845.mk
+++ sdm845.mk
@@ -309,10 +309,6 @@ PRODUCT_PACKAGES += \
     libjson \
     librmnetctl
 
-# Recovery
-PRODUCT_PACKAGES += \
-    librecovery_updater_xiaomi
-
 # RenderScript
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
