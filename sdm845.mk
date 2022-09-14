65c0ed50d346cbff06da562407671306eb2f96af
Bruno Martins <bgcngm@gmail.com>
2022-09-14 14:51:15 +0100

    sdm845-common: Migrate to AIDL ClearKey DRM HAL
    Change-Id: Iad5cc1176978137c647069197b33f49fb4d98ab2

--- sdm845.mk
+++ sdm845.mk
@@ -159,8 +159,8 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.3.vendor \
-    android.hardware.drm@1.4-service.clearkey
+    android.hardware.drm-service.clearkey \
+    android.hardware.drm@1.3.vendor
 
 # Fingerprint
 PRODUCT_PACKAGES += \
