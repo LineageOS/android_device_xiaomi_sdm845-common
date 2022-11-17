c25da137dbf0e167cb9a63cab50524bbf831c286
Bruno Martins <bgcngm@gmail.com>
2022-11-17 14:22:49 +0000

    Revert "sdm845-common: Do not enable updatable APEXes on every target"
    This reverts commit 6b1e8d7d968f3b5dfb2bdee100fc906961c544bd.
    
    All targets can now support it, since FDE support is dead
    and beryllium was migrated to FBE.
    
    Change-Id: Ic29619179207aab4e1bbebb8897da0a27036b4bb

--- sdm845.mk
+++ sdm845.mk
@@ -8,6 +8,9 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
