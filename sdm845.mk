5a17c028985662202a053a61f7b9e7a9921e3d04
Michael Bestas <mkbestas@lineageos.org>
2023-03-03 23:43:42 +0200

    sdm845-common: Build Lineage Health HAL
    Change-Id: I70cc903a03ac9fa3303bb8fa039c9de4806449bf

--- sdm845.mk
+++ sdm845.mk
@@ -221,6 +221,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.xiaomi_sdm845
 
+# Lineage Health
+PRODUCT_PACKAGES += \
+    vendor.lineage.health-service.default
+
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service-sdm
