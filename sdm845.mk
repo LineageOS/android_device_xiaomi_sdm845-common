16dcf9f20df552b8a3ea494b2ecb6df073554a14
Sebastiano Barezzi <seba@sebaubuntu.dev>
2023-04-13 18:39:43 +0200

    sdm845-common: Build Xiaomi sensors impl
    Change-Id: Ib477ac016c312ce02e9f71df2a7d1d0fcca717e8

--- sdm845.mk
+++ sdm845.mk
@@ -257,7 +257,7 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl:64 \
+    android.hardware.sensors@1.0-impl-xiaomi \
     android.hardware.sensors@1.0-service
 
 # Soong namespaces
