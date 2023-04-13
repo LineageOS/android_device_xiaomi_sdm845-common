bab264e139bdeedad00f4149dd4d02d568b55de2
Sebastiano Barezzi <seba@sebaubuntu.dev>
2023-04-14 01:23:39 +0200

    sdm845-common: Build Xiaomi sensors impl
    Change-Id: Ib477ac016c312ce02e9f71df2a7d1d0fcca717e8

--- sdm845.mk
+++ sdm845.mk
@@ -318,7 +318,7 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl:64 \
+    android.hardware.sensors@1.0-impl-xiaomi \
     android.hardware.sensors@1.0-service \
     libsensorndkbridge
 
