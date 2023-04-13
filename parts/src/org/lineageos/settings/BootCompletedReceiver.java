5641c9f3d719abb60b215c12e30d79746aeabe77
Sebastiano Barezzi <seba@sebaubuntu.dev>
2023-04-14 01:27:16 +0200

    sdm845-common: parts: Drop doze related features
    Change-Id: I01c86719c6a9608fef6c24504325f603af6f0d9e

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -23,7 +23,6 @@ import android.content.Intent;
 import android.util.Log;
 
 import org.lineageos.settings.dirac.DiracUtils;
-import org.lineageos.settings.doze.DozeUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
@@ -35,7 +34,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
     public void onReceive(final Context context, Intent intent) {
         if (DEBUG) Log.d(TAG, "Received boot completed intent");
         DiracUtils.initialize();
-        DozeUtils.checkDozeService(context);
         ThermalUtils.startService(context);
     }
 }
