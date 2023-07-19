e0788d24424ac2130e32758a54e5ffabe9d9440e
dianlujitao <dianlujitao@lineageos.org>
2023-07-19 15:55:06 +0200

    sdm845-common: Make FM radio opt-in
    Change-Id: Idaebe34e18d16d042e247f0f2b06c8012fe4d287

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -65,6 +65,9 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml
 
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
+ifeq ($(TARGET_HAS_FM),true)
+DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest_fm.xml
+endif
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 
 # LMKD
