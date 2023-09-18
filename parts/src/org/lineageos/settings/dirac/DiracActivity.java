607ee4a024b2640eb4ed371eec10981bf994693d
Peter Kalauskas <peskal@google.com>
2023-09-18 22:10:57 +0000

    sdm845-common: parts: Enable use_resource_processor for all sysui deps
    This will make the build faster
    
    Test: m checkbuild
    Bug: 295208392
    Change-Id: I0c1bd901429bbe3bf81c1530e156735f8637a96e

--- parts/src/org/lineageos/settings/dirac/DiracActivity.java
+++ parts/src/org/lineageos/settings/dirac/DiracActivity.java
@@ -19,7 +19,6 @@ package org.lineageos.settings.dirac;
 import android.os.Bundle;
 
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
-import com.android.settingslib.widget.R;
 
 public class DiracActivity extends CollapsingToolbarBaseActivity {
 
@@ -29,7 +28,8 @@ public class DiracActivity extends CollapsingToolbarBaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        getFragmentManager().beginTransaction().replace(R.id.content_frame,
+        getFragmentManager().beginTransaction().replace(
+                com.android.settingslib.collapsingtoolbar.R.id.content_frame,
                 new DiracSettingsFragment(), TAG_DIRAC).commit();
     }
 }
