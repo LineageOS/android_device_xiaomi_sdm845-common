131095252bdf7e1d3fab05bd8033deeef9c91d33
PainKiller3 <ninadpatil100@gmail.com>
2023-11-06 12:08:06 +0530

    sdm845-common: Kang display blobs from munch
    Co-authored-by: Edwin Moquete <edwinmmoquete@gmail.com>
    Change-Id: I98a9ffd3aefcbf49cf0a02fee4005d137e3e6163

--- sdm845.mk
+++ sdm845.mk
@@ -140,12 +140,12 @@ PRODUCT_PACKAGES += \
 
 # Display
 PRODUCT_PACKAGES += \
-    android.hardware.graphics.composer@2.3-service \
-    android.hardware.graphics.mapper@2.0-impl-qti-display \
-    gralloc.sdm845 \
-    hwcomposer.qcom \
-    vendor.qti.hardware.display.allocator@1.0-service \
-    vendor.qti.hardware.memtrack-service
+    vendor.qti.hardware.display.composer-service \
+    android.hardware.graphics.mapper@3.0-impl-qti-display \
+    android.hardware.graphics.mapper@4.0-impl-qti-display \
+    vendor.qti.hardware.display.allocator-service \
+    vendor.qti.hardware.memtrack-service \
+    gralloc.qcom
 
 # DRM
 PRODUCT_PACKAGES += \
