9541db103cf7bfbc2e165f432416a796b139b978
PainKiller3 <ninadpatil100@gmail.com>
2023-11-07 15:07:02 +0530

    sdm845-common: Update CNE, DPM, IMS, QMI and RIL blobs from munch
    Change-Id: I12f1f5d2c626c046390031bbcf53fb99abb2a7fc

--- sdm845.mk
+++ sdm845.mk
@@ -237,7 +237,8 @@ PRODUCT_COPY_FILES += \
 
 # QTI fwk-detect
 PRODUCT_PACKAGES += \
-    libqti_vndfwk_detect.vendor:32
+    libqti_vndfwk_detect.vendor \
+    libvndfwk_detect_jni.qti.vendor # Needed by CNE app
 
 # Recovery
 PRODUCT_PACKAGES += \
