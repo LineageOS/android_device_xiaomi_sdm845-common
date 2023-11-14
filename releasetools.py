7bdd617802f4526384cd8af35a580cf8818c5ce0
Bruno Martins <bgcngm@gmail.com>
2023-11-14 14:15:37 +0000

    sdm845-common: releasetools: Take reqs from android-info-extra.txt
    Change-Id: If14788b6c0a70d6af08ed1d6f95c00edb99d2be2

--- releasetools.py
+++ releasetools.py
@@ -1,6 +1,6 @@
 # Copyright (C) 2009 The Android Open Source Project
 # Copyright (c) 2011, The Linux Foundation. All rights reserved.
-# Copyright (C) 2017-2022 The LineageOS Project
+# Copyright (C) 2017-2023 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -49,7 +49,7 @@ def OTA_InstallEnd(info):
   return
 
 def AddTrustZoneAssertion(info, input_zip):
-  android_info = info.input_zip.read("OTA/android-info.txt")
+  android_info = info.input_zip.read("OTA/android-info-extra.txt")
   m = re.search(r'require\s+version-trustzone\s*=\s*(\S+)', android_info.decode('utf-8'))
   if m:
     versions = m.group(1).split('|')
