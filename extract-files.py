beff4be251425c831a32036c3bb776fc482f0050
Michael Bestas <mkbestas@lineageos.org>
2024-08-25 21:46:46 +0300

    sdm845-common: Patch libwfdservice to resolve duplicate dependencies
    Fixes:
    error: module "libwfdservice" variant
    "android_arm_armv8-a_cortex-a76_static": depends on multiple
    versions of the same aidl_interface:
    android.media.audio.common.types-V2-cpp-source,
    android.media.audio.common.types-V4-cpp-source
    
    Change-Id: I8086a80fcc238d6b03d16640ee2c4e134129bbab

--- extract-files.py
+++ extract-files.py
@@ -66,6 +66,8 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('libgui_shim.so'),
     'system_ext/lib/libwfdmmsrc_system.so': blob_fixup()
         .add_needed('libgui_shim.so'),
+    'system_ext/lib/libwfdservice.so': blob_fixup()
+        .replace_needed('android.media.audio.common.types-V2-cpp.so', 'android.media.audio.common.types-V4-cpp.so'),
     'system_ext/lib64/libwfdnative.so': blob_fixup()
         .add_needed('libbinder_shim.so')
         .add_needed('libinput_shim.so'),
