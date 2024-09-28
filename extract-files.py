1d40f6917196f15c53fe6c49a9de61032b9262c0
Bruno Martins <bgcngm@gmail.com>
2024-09-28 16:30:33 +0100

    sdm845-common: Setup ELF checks
    Co-authored-by: Aaron Kling <webgeek1234@gmail.com>
    Co-authored-by: Cosmin Tanislav <demonsingur@gmail.com>
    Co-authored-by: Michael Bestas <mkbestas@lineageos.org>
    Change-Id: I09130799d80e7abb8b694f5b8fe7266a5ea74fb4

--- extract-files.py
+++ extract-files.py
@@ -8,10 +8,54 @@ from extract_utils.fixups_blob import (
     blob_fixup,
     blob_fixups_user_type,
 )
+from extract_utils.fixups_lib import (
+    lib_fixups,
+    lib_fixups_user_type,
+)
 from extract_utils.main import (
     ExtractUtils,
     ExtractUtilsModule,
 )
+
+namespace_imports = [
+    'device/xiaomi/sdm845-common',
+    'hardware/qcom-caf/sdm845',
+    'hardware/qcom-caf/wlan',
+    'hardware/xiaomi',
+    'vendor/qcom/opensource/commonsys-intf/display',
+    'vendor/qcom/opensource/commonsys/display',
+    'vendor/qcom/opensource/dataservices',
+    'vendor/qcom/opensource/display',
+]
+
+
+def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
+    return f'{lib}_{partition}' if partition == 'vendor' else None
+
+
+lib_fixups: lib_fixups_user_type = {
+    **lib_fixups,
+    (
+        'com.qualcomm.qti.dpm.api@1.0',
+        'com.qualcomm.qti.imscmservice@1.0',
+        'com.qualcomm.qti.imscmservice@2.0',
+        'com.qualcomm.qti.imscmservice@2.1',
+        'com.qualcomm.qti.imscmservice@2.2',
+        'com.qualcomm.qti.uceservice@2.0',
+        'com.qualcomm.qti.uceservice@2.1',
+        'libmmosal',
+        'vendor.qti.hardware.radio.ims@1.0',
+        'vendor.qti.hardware.radio.ims@1.1',
+        'vendor.qti.hardware.radio.ims@1.2',
+        'vendor.qti.hardware.radio.ims@1.3',
+        'vendor.qti.hardware.radio.ims@1.4',
+        'vendor.qti.hardware.wifidisplaysession@1.0',
+        'vendor.qti.ims.callinfo@1.0',
+        'vendor.qti.ims.rcsconfig@1.0',
+        'vendor.qti.imsrtpservice@2.0',
+        'vendor.qti.imsrtpservice@2.1',
+    ): lib_fixup_vendor_suffix,
+}
 
 blob_fixups: blob_fixups_user_type = {
     'system_ext/etc/permissions/qcrilhook.xml': blob_fixup()
@@ -30,7 +74,8 @@ module = ExtractUtilsModule(
     'sdm845-common',
     'xiaomi',
     blob_fixups=blob_fixups,
-    check_elf=False,
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
 )
 
 if __name__ == '__main__':
