3d75aa91b21172c115ec475519bff402c7a4c6e2
basamaryan <basam.aryan@gmail.com>
2024-09-29 16:24:14 -0400

    sdm845-common: Move to Xiaomi fingerprint AIDL
    Change-Id: I68dba7184034fd37454c7d32a065f041cb611625

--- sdm845.mk
+++ sdm845.mk
@@ -167,7 +167,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.xiaomi
+    android.hardware.biometrics.fingerprint-service.xiaomi
 
 # Gatekeeper
 PRODUCT_PACKAGES += \
