236f4cfa343348d6cb85ab8bb5cd145336b99d07
Gérard Parat <github@le-cycliste.fr>
2025-01-07 22:53:02 +0100

    sdm845-common: Switch to python extract-utils
    Change-Id: Ifad8b3065537a80acef5f391976374f6b582b430

---
+++ extract-files.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2025 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+blob_fixups: blob_fixups_user_type = {
+    'system_ext/etc/permissions/qcrilhook.xml': blob_fixup()
+        .regex_replace('/product/framework/', '/system_ext/framework/'),
+    'system_ext/etc/permissions/qti_libpermissions.xml': blob_fixup()
+        .regex_replace('name="android.hidl.manager-V1.0-java', 'name="android.hidl.manager@1.0-java'),
+    'system_ext/lib64/lib-imsvideocodec.so': blob_fixup()
+        .add_needed('libgui_shim.so'),
+    'vendor/bin/pm-service': blob_fixup()
+        .add_needed('libutils-v33.so'),
+    'vendor/lib/camera/components/com.qti.node.watermark.so': blob_fixup()
+        .add_needed('libpiex_shim.so'),
+    'vendor/lib64/libwvhidl.so|vendor/lib64/mediadrm/libwvdrmengine.so': blob_fixup()
+        .add_needed('libcrypto_shim.so'),
+}  # fmt: skip
+
+module = ExtractUtilsModule(
+    'sdm845-common',
+    'xiaomi',
+    blob_fixups=blob_fixups,
+    check_elf=False,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device(module)
+    utils.run()
