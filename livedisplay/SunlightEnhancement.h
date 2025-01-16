b10d6c52005863aaa10e19437dad1fdb34840d22
Bruno Martins <bgcngm@gmail.com>
2025-01-16 08:43:06 +0000

    sdm845-common: livedisplay: Use pragma once directive
    Change-Id: Icb26f68f769ccd202d415d6e4df3568694514fc1

--- livedisplay/SunlightEnhancement.h
+++ livedisplay/SunlightEnhancement.h
@@ -14,8 +14,7 @@
  * limitations under the License.
  */
 
-#ifndef VENDOR_LINEAGE_LIVEDISPLAY_V2_0_SUNLIGHTENHANCEMENT_H
-#define VENDOR_LINEAGE_LIVEDISPLAY_V2_0_SUNLIGHTENHANCEMENT_H
+#pragma once
 
 #include <hidl/MQDescriptor.h>
 #include <hidl/Status.h>
@@ -45,5 +44,3 @@ class SunlightEnhancement : public ISunlightEnhancement {
 }  // namespace livedisplay
 }  // namespace lineage
 }  // namespace vendor
-
-#endif  // VENDOR_LINEAGE_LIVEDISPLAY_V2_0_SUNLIGHTENHANCEMENT_H
