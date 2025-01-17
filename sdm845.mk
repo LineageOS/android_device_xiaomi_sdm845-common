c26e3a25d70d1d542c6b506590c49d790cd1d597
LuK1337 <priv.luk@gmail.com>
2025-01-17 17:06:51 +0100

    sdm845-common: Switch to common lights HAL
    Change-Id: If515e7c3f6e5b6895076f1d8381fb2c22bd1b3de

--- sdm845.mk
+++ sdm845.mk
@@ -222,7 +222,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light-service.xiaomi
+    android.hardware.light-service.lineage
 
 # Lineage Health
 PRODUCT_PACKAGES += \
