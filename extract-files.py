ffc7053da6d4c66ff42b10ecdb17f332986ec20e
Bruno Martins <bgcngm@gmail.com>
2025-04-04 21:09:02 +0100

    sdm845-common: Downgrade WFD system stack
    This is the latest known to be good stack on these target
    while running V QPR2.
    
    Change-Id: I3c6af7bf6368420693797ea194fd56701b45b2be

--- extract-files.py
+++ extract-files.py
@@ -20,6 +20,11 @@ blob_fixups: blob_fixups_user_type = {
         .regex_replace('name="android.hidl.manager-V1.0-java', 'name="android.hidl.manager@1.0-java'),
     'system_ext/lib64/lib-imsvideocodec.so': blob_fixup()
         .add_needed('libgui_shim.so'),
+    'system_ext/lib/libwfdmmsrc_system.so': blob_fixup()
+        .add_needed('libgui_shim.so'),
+    'system_ext/lib64/libwfdnative.so': blob_fixup()
+        .add_needed('libbinder_shim.so')
+        .add_needed('libinput_shim.so'),
     'vendor/bin/pm-service': blob_fixup()
         .add_needed('libutils-v33.so'),
     ('vendor/lib64/libwvhidl.so', 'vendor/lib64/mediadrm/libwvdrmengine.so'): blob_fixup()
