406458576ab7fb440c21325fea0db649c3358661
duckyduckG <duckyduckg65@gmail.com>
2025-07-26 18:55:18 +0500

    sdm845-common: rro: Remove tethering overlay
    Tethering package provides sane defaults anyway.
    
    Change-Id: I0cdd4b143de182cb4b80056e24862bcf6bb7f5e2
    Signed-off-by: duckyduckG <duckyduckg65@gmail.com>

--- sdm845.mk
+++ sdm845.mk
@@ -275,10 +275,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
-# Tethering
-PRODUCT_PACKAGES += \
-    TetheringConfigOverlay
-
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service.dual_role_usb
