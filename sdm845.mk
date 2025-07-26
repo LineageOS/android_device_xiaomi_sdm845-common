030a8e6c2ffd1df8be4d52e12cf8869dab399bc3
LuK1337 <priv.luk@gmail.com>
2025-07-26 18:57:19 +0500

    sdm845-common: Switch to NCM USB tethering
    Change-Id: I20b6b2b2e26a45d8b28c7483afc83ad4da4b8643
    Signed-off-by: duckyduckG <duckyduckg65@gmail.com>

--- sdm845.mk
+++ sdm845.mk
@@ -23,6 +23,9 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
+PRODUCT_PACKAGES += \
+    NcmTetheringOverlay
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
