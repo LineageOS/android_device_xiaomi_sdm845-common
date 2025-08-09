1f0de3e9b36b72ff9fef1ef4aefa4f9ab9551166
Edwin Moquete <edwinmmoquete@gmail.com>
2025-08-09 18:03:48 +0500

    sdm845-common: Drop unused system_properties.xml Only used for OMX codecs
    Change-Id: I3a03599fc65ab5488fde20031c073d1134c7d7ea

--- sdm845.mk
+++ sdm845.mk
@@ -199,8 +199,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs_performance_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance_c2.xml \
     $(LOCAL_PATH)/configs/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles.xml \
     $(LOCAL_PATH)/configs/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml \
-    $(LOCAL_PATH)/configs/media_profiles_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_vendor.xml \
-    $(LOCAL_PATH)/configs/system_properties.xml:$(TARGET_COPY_OUT_VENDOR)/etc/system_properties.xml
+    $(LOCAL_PATH)/configs/media_profiles_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_vendor.xml
 
 # Native libraries whitelist
 PRODUCT_COPY_FILES += \
