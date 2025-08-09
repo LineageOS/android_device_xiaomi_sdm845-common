4c348b1b6ada6df2df603b806580bd1718fc43f3
LuK1337 <priv.luk@gmail.com>
2025-08-09 17:52:11 +0500

    sdm845-common: Move media_codecs_c2.xml to device tree
    Change-Id: Ib2ebfff4281eafd46427180c2666a71d0d144300

--- sdm845.mk
+++ sdm845.mk
@@ -195,6 +195,7 @@ PRODUCT_PACKAGES += \
 
 # Media
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/media_codecs_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2.xml \
     $(LOCAL_PATH)/configs/media_codecs_performance_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance_c2.xml \
     $(LOCAL_PATH)/configs/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles.xml \
     $(LOCAL_PATH)/configs/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml \
