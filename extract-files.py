16047124148b264d04ceaa2024c8d7cc8a922eb4
basamaryan <basam.aryan@gmail.com>
2025-09-12 21:21:24 +0500

    sdm845-common: Patch audio to load libaudioroute-v34.so
    Change-Id: Idad20512982b8753820fffa6d956df137e462638

--- extract-files.py
+++ extract-files.py
@@ -50,6 +50,8 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('libutils-v33.so'),
     ('vendor/lib64/libwvhidl.so', 'vendor/lib64/mediadrm/libwvdrmengine.so'): blob_fixup()
         .add_needed('libcrypto_shim.so'),
+    ('vendor/lib/hw/audio.primary.sdm845.so', 'vendor/lib/hw/sound_trigger.primary.sdm845.so', 'vendor/lib/libaudioroute_ext.so'): blob_fixup()
+        .replace_needed('libaudioroute.so', 'libaudioroute-v34.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
