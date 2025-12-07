d8aa9b1a905f41d7fce39a71a1aec9d976cd47b4
Bruno Martins <bgcngm@gmail.com>
2025-12-07 09:40:50 +0000

    sdm845-common: parts: Clean up imports
    Change-Id: I483a137865e0e558ecadfb1ed25d0f46daf0f3b2

--- parts/src/org/lineageos/settings/doze/DozeSettingsFragment.java
+++ parts/src/org/lineageos/settings/doze/DozeSettingsFragment.java
@@ -18,10 +18,7 @@
 package org.lineageos.settings.doze;
 
 import android.app.AlertDialog;
-import android.app.Dialog;
-import android.app.DialogFragment;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
--- parts/src/org/lineageos/settings/doze/DozeUtils.java
+++ parts/src/org/lineageos/settings/doze/DozeUtils.java
@@ -17,6 +17,9 @@
 
 package org.lineageos.settings.doze;
 
+import static android.provider.Settings.Secure.DOZE_ALWAYS_ON;
+import static android.provider.Settings.Secure.DOZE_ENABLED;
+
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -28,9 +31,6 @@ import android.provider.Settings;
 import android.util.Log;
 
 import androidx.preference.PreferenceManager;
-
-import static android.provider.Settings.Secure.DOZE_ALWAYS_ON;
-import static android.provider.Settings.Secure.DOZE_ENABLED;
 
 public final class DozeUtils {
 
