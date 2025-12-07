fbbaeb386cf9ff61d66fc9044e3729b6036b5124
Bruno Martins <bgcngm@gmail.com>
2025-12-07 10:08:32 +0000

    sdm845-common: parts: Replace Activity.MODE_PRIVATE with Context.MODE_PRIVATE
    Activity.MODE_PRIVATE is redundant because Activity is already a Context.
    
    Change-Id: Ib2e751de93294efe251fd5af272be27438b50025

--- parts/src/org/lineageos/settings/doze/DozeSettingsFragment.java
+++ parts/src/org/lineageos/settings/doze/DozeSettingsFragment.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2015 The CyanogenMod Project
- *               2017-2023 The LineageOS Project
+ *               2017-2025 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,7 +17,6 @@
 
 package org.lineageos.settings.doze;
 
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
@@ -55,7 +54,7 @@ public class DozeSettingsFragment extends PreferenceFragmentCompat implements
         setPreferencesFromResource(R.xml.doze_settings, rootKey);
 
         SharedPreferences prefs = getActivity().getSharedPreferences("doze_settings",
-                Activity.MODE_PRIVATE);
+                Context.MODE_PRIVATE);
         if (savedInstanceState == null && !prefs.getBoolean("first_help_shown", false)) {
             showHelp();
         }
@@ -138,7 +137,7 @@ public class DozeSettingsFragment extends PreferenceFragmentCompat implements
                 .setPositiveButton(R.string.dialog_ok,
                         (dialog, which) -> {
                             getActivity()
-                                    .getSharedPreferences("doze_settings", Activity.MODE_PRIVATE)
+                                    .getSharedPreferences("doze_settings", Context.MODE_PRIVATE)
                                     .edit()
                                     .putBoolean("first_help_shown", true)
                                     .commit();
